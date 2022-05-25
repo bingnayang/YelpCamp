dade65e35cc17d2405f61e6667cbb04f6ede0ec3
BingNa Yang <bing334@gmail.com>
2022-05-25 11:23:37 -0400

    Defining ExpressError class and catchAsync function
    - use catchAsync function for async instead of try-catch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -33,10 +34,10 @@ app.get('/', (req, res) => {
 })
 
 // Route for /campgrounds
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
-})
+}))
 
 // Route for /campgrounds/new
 app.get('/campgrounds/new', (req, res) => {
@@ -45,41 +46,37 @@ app.get('/campgrounds/new', (req, res) => {
 
 // Post route for /campgrounds
 // Redirect back to /campgrounds/:id after post
-app.post('/campgrounds', async (req, res, next) => {
-    try{
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`)
-    }catch(e){
-        next(e)
-    }
-})
+}))
 
 // Route for /campgrounds/:id
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', {campground});
-})
+}))
 
 // Route for /campgrounds/:id/edit
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', {campground});
-})
+}))
 
 // Route for update campground 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // Route for delete campground 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect(`/campgrounds`)
-})
+}))
 
 // Error Handle
 app.use((err, req, res, next) => {
