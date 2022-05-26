9b3eb7dfef967ba0a83b96339638a2b898be3240
BingNa Yang <bing334@gmail.com>
2022-05-26 18:38:59 -0400

    use JOI Schema validations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate')
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const expressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
@@ -48,8 +49,24 @@ app.get('/campgrounds/new', (req, res) => {
 // Post route for /campgrounds
 // Redirect back to /campgrounds/:id after post
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-        if(!req.body.campground) throw new expressError('Invalid Campground Data', 400);
+        // if(!req.body.campground) throw new expressError('Invalid Campground Data', 400);
+        const campgroundSchema = Joi.object({
+            campground: Joi.object({
+                title: Joi.string().required(),
+                price: Joi.number().required().min(0),
+                image: Joi.string().required(),
+                location: Joi.string().required(),
+                description: Joi.string().required()
+            }).required()
+        })
+        const { error } = campgroundSchema.validate(req.body);
+        
+        if(error){
+            const msg = error.details.map(el => el.message).join(',')
+            throw new expressError(msg, 400)
+        }
 
+        console.log(result);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`)
