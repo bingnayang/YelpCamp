8c1db977e4c340aad0018443a60f73e7222f0274
BingNa Yang <bing334@gmail.com>
2022-06-13 17:55:45 -0400

    ReturnTo behavior
    - get original Url
    - update POST to redirect to original url or '/campgrounds'

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // Middleware for flash
 app.use((req, res, next) => {
+    console.log(req.session);
     res.locals.currentUser = req.user; // Get current login user
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error')
