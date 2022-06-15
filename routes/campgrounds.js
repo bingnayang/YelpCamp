6f63b94b02ec32c5fa668a7cc825a541346e64b0
BingNa Yang <bing334@gmail.com>
2022-06-15 19:30:35 -0400

    Display review author
    - populate review author
    - display author name in each review

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -31,7 +31,12 @@ router.post('/', isLoggedIn,validateCampground, catchAsync(async (req, res, next
 // Route for /campgrounds/:id
 // Populate review
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id)
+        .populate({
+            path:'reviews', 
+            populate:{path:'author'}
+        })
+        .populate('author');
     console.log(campground);
     // Call flash if campground not found
     // Redirect to campgrounds page
